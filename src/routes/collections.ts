2c6dcb0022f86a873fb390f6548f81b561ecc73c
Brittany Ellich <brittanyellich@github.com>
2025-11-29 13:35:43 -0800

    Refactoring

--- src/routes/collections.ts
+++ src/routes/collections.ts
@@ -1,46 +1,12 @@
 import express, { Request, Response } from 'express';
-import { getIronSession } from 'iron-session';
 import { Agent } from '@atproto/api';
 import type { AppContext } from '../context';
-import { config } from '../config';
 import { handler } from '../lib/http';
 import {
   AppCollectiveSocialList,
   AppCollectiveSocialListitem,
 } from '../types/lexicon';
-
-type Session = { did?: string };
-
-// Helper function to get the Atproto Agent for the active session
-async function getSessionAgent(
-  req: express.Request,
-  res: express.Response,
-  ctx: AppContext
-) {
-  res.setHeader('Vary', 'Cookie');
-
-  const session = await getIronSession<Session>(req, res, {
-    cookieName: 'sid',
-    password: config.cookieSecret,
-    cookieOptions: {
-      secure: config.nodeEnv === 'production',
-      sameSite: 'lax',
-      httpOnly: true,
-      path: '/',
-    },
-  });
-  if (!session.did) return null;
-
-  res.setHeader('cache-control', 'private, no-store');
-
-  try {
-    const oauthSession = await ctx.oauthClient.restore(session.did);
-    return oauthSession ? new Agent(oauthSession) : null;
-  } catch (err) {
-    await session.destroy();
-    return null;
-  }
-}
+import { getSessionAgent } from '../auth/agent';
 
 export const createRouter = (ctx: AppContext) => {
   const router = express.Router();
